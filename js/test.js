e80b3e88541cc30490b90b188921bf6dda373975
zhangyuhan <hi_zhangyuhan@163.com>
2018-03-21 15:44:10 +0800

    控制并发

--- js/test.js
+++ js/test.js
@@ -17,7 +17,6 @@ function downText() {
   /* 设置 */
   $('#top_tip').text('请等待我上面出现下载链接')
   getAllTxt()
-
 }
 
 /* 生成下载链接 */
@@ -30,33 +29,103 @@ function creatLink() {
   a.href = URL.createObjectURL(file)
   a.download = name
   $('#downBook').show()
+  document.querySelector('#my_list').scrollTop = 0
+  $("#cc_box").css('position','initial')
+  alert('文件生成成功,请前往下载')
+
 }
 
+/* 变色 */
+function changeColor(i,f = true){
+  $('#my_list input:checked').eq(i).siblings('span').css({
+    'color': f ? 'green' : 'red'
+  })
+}
+
+/* ajax */
+let sArr = []
+let eArr = []
+let titleArr = []
+let aNumber = 0
 /* Ajax获取所选的章节内容 */
 function getAllTxt() {
   let arr = []
-  let titleArr = []
-  $('#my_list input:checked').each(function () {
+  titleArr = []
+  sArr = []
+  eArr = []
+
+  aNumber = $('#my_list input:checked').length
+  $('#my_list input:checked').each(function (index) {
     let url = $(this).siblings('span').attr('data-url')
     titleArr.push($(this).siblings('span').text())
-    arr.push(createGet(url))
+    let i = createGet(url)
+    arr.push(i)
+    /* ajax 获取HTML*/
+    getH(i,index)
   })
-  axios.all(arr)
-    .then(res => {
-      let tt = document.createElement('div')
-      tt.id = 'my_book_txt'
-      let tempData = ''
-      for (let i = 0; i < res.length; i++) {
-        tempData += '\r\n' + titleArr[i].replace(/\s/g, '^^^') + '\r\n' + getContent(res[i].data)
-      }
-      tt.innerHTML = tempData
-      $('body').append(tt)
-      /* 生成下载链接 */
-      creatLink()
-    }).catch(err => {
-      alert('对不起,下载失败')
-      console.log('--err--', err)
-    })
+  /* 未控制版 */
+  // axios.all(arr)
+  //   .then(res => {
+  //     let tt = document.createElement('div')
+  //     tt.id = 'my_book_txt'
+  //     let tempData = ''
+  //     for (let i = 0; i < res.length; i++) {
+  //       tempData += '\r\n' + titleArr[i].replace(/\s/g, '^^^') + '\r\n' + getContent(res[i].data)
+  //     }
+  //     tt.innerHTML = tempData
+  //     $('body').append(tt)
+  //     /* 生成下载链接 */
+  //     creatLink()
+  //   }).catch(err => {
+  //     alert('对不起,下载失败')
+  //     console.log('--err--', err)
+  //   })
+}
+/* 生成HTML */
+function setH (res) {
+  console.log('--arr--', res)
+  let tt = document.createElement('div')
+  tt.id = 'my_book_txt'
+  let tempData = ''
+  for (let i = 0; i < res.length; i++) {
+    tempData += '\r\n' + titleArr[i].replace(/\s/g, '^^^') + '\r\n' + getContent(res[i].data)
+  }
+  tt.innerHTML = tempData
+  $('body').append(tt)
+  /* 生成下载链接 */
+  creatLink()
+}
+/* 重试次数 */
+let sObj = {}
+/* 获取HTML */
+function getH(i,index){
+  sObj[index] = (sObj[index] || 0) + 1
+  if(sObj[index] > 4){
+    return false
+  }
+  setTimeout(()=>{
+    i.get()
+      .then(res=> {
+        changeColor(index)
+        sArr[index] = res
+        // console.log(`第${index}章 ${titleArr[index]} *`)
+        if(eArr.includes(index)){
+          eArr.splice(eArr.indexOf(index),1)
+        }
+        if(sArr.filter(v=>v).length === aNumber){
+          // console.log('--全部获取--', sArr,eArr)
+          return setH(sArr)
+        }
+        if(eArr.length){
+          // console.log('--获取失败--', eArr,sArr)
+        }
+      })
+      .catch(err => {
+        changeColor(index,false)
+        eArr.push(index)
+        return getH(i,index)
+      })
+  },1000 * (Math.random() * 10 + 1))
 }
 
 /* 生成目录选择 */
@@ -65,7 +134,7 @@ function createList() {
   let tempList = `<div id="cc_box">
         <button id="createBook">创建txt</button>
         <a style="display: none;" href="javascript:alert('创建失败')" id="downBook">点击下载电子书</a>
-    </div><div id="top_tip">将下载勾选的章节到txt中</div><label for="checkInput">全选</label><input type="checkbox" checked id="checkInput">`
+    </div><div id="top_tip">将下载勾选的章节到txt中</div><label for="checkInput">全选</label><input type="checkbox" checked id="checkInput"><input type="number" name="q_n">-<input type="number" name="q_n"><button id="q_button">节选</button>`
   $('#list a').each(function () {
     let u = null
     try {
@@ -83,10 +152,24 @@ function createList() {
     toggleStart(true)
     downText()
   })
+  /* 全选 */
   $("#checkInput").on('click', function () {
     let flag = $(this).prop('checked')
     $(".my_book_box input").prop('checked', flag)
   })
+  /* 节选 */
+  $("#q_button").on('click',function () {
+    $("#checkInput").prop('checked',false)
+    $(".my_book_box input").prop('checked', false)
+    let tempArr = [$("input[name='q_n']").eq(0).val() || 0,$("input[name='q_n']").eq(1).val() || 0]
+    goTop(tempArr[0])
+    checkSome(tempArr[0],tempArr[1])
+  })
+}
+/* mark */
+function goTop(i){
+  var temp = document.querySelectorAll('#my_list span')[i]
+  document.querySelector('#my_list').scrollTop = temp.offsetTop
 }
 /* 节选 */
 function checkSome(min,max){
@@ -164,17 +247,24 @@ function addMyClass() {
   /* 添加tip */
   $('#top_tip').attr('style', `text-align: center;color: #3cd104;padding: 25px 0;`)
   /* 操作 */
-  $('#cc_box').attr('style', `display: flex;
+  $('#cc_box').attr('style', `display: flex;position: fixed;
     flex-direction: column;
     align-items: center;`)
-  $('#cc_box button').attr('style', `min-width: 100px;
+  $('#cc_box button').attr('style', `min-width: 100px;background-color: khaki;
     font-size: 16px;
     margin-bottom: 10px;height: 30px;`)
+  /* 添加节选 */
+  $("input[name='q_n']").attr('style',`max-width: 3rem`)
+  $("#q_button").attr('style',`margin-left: 0.5rem;`)
+  /* 下载高亮 */
+  $("#downBook").attr('style',`display:none;font-size: 1.5rem;font-weight: bold;color: cornflowerblue;`)
 }
 /* 创建axios */
 function createGet(url) {
   let api = location.href
-  return axios.get(api + url)
+  return axios.create({
+    baseURL: api + url
+  })
 }
 /* txt阅读 */
 function lookTxt() {
